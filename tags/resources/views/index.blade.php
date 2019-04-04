8274cef18059856e5084c43dedfed0bf1c1dbcf5
Rodrigo Barbosa <vanderdigo837@outlook.com>
2019-04-04 19:40:52 -0300

    Materialize na view e tags inputs

--- tags/resources/views/index.blade.php
+++ tags/resources/views/index.blade.php
@@ -4,14 +4,39 @@
         <meta charset="utf-8">
 
         <title>Tags</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+        <!-- Compiled and minified CSS -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
 
     </head>
     <body>
+        <div class="container">
+            <div class="row">
+                <div>
+                    <h1 class="">Sistema de etiquetas</h1>
+                </div>
+                <br>
+
+                <hr>
+                @foreach ($posts as $post)
+                    <div class="">{{ $post->title }}</div>
+                    <div class="">{{ $post->body }}</div>
+                    <div class="panel-footer">
+                        @forelse ($post->tags as $tag)
+                            <div class="chip">
+                                    {{ $tag->name }}
+                                    <i class="close material-icons">close</i>
+                            </div>
+                            @empty
+                                <em>Sem etiquetas</em>
+                            @endempty
+                        @endforelse
+                    </div>
+                @endforeach
+            </div>
+        </div>
 
 
-
+            <!-- Compiled and minified JavaScript -->
+     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
     </body>
 </html>
